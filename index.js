c85463a69855d71eb137513915092cd1b10c1a38
Bryan <bryancabreram3@gmail.com>
2023-03-16 12:58:01 -0300

    Mostrando en página index un formulario con los datos requeridos

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require("fs");
 const app = express()
-const json = JSON.parse(fs.readFileSync('equipos.json', {encoding:"utf-8"}))
+const path = require('path')
+let pilotos = JSON.parse(fs.readFileSync('equipos.json', {encoding:"utf-8"}))
+const circuitos = JSON.parse(fs.readFileSync('circuitos.json', {encoding:"utf-8"}))
 
 app.set('view engine', 'hbs')
 app.set('views', './views')
+app.use(express.static(path.join(__dirname , 'node_modules/bootstrap/dist')))
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper("suma", function(value, options) {return parseInt(value) + 1})
 app.listen(3000)
 
-app.get('/', (req, res) => res.render('index', {resultado: json}))
+const pilotos1 = pilotos.map(equipo => equipo.piloto1)
+const pilotos2 = pilotos.map(equipo => equipo.piloto2)
+pilotos = (pilotos1.concat(...pilotos2))
+
+app.get('/', (req, res) => res.render('index', {pilotos: pilotos, carreras: circuitos}))
 
 // Ruta para mostrar un mensaje si la página no se encontró
 app.all("*", (req, res) => res.status(404).send("Página no encontrada"))
