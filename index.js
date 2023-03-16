facec1856917019b1973eeb9c6a5bff842a7d406
Esteban <eban316@gmail.com>
2023-03-16 13:05:31 -0300

    Pruebas

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ const fs = require("fs");
 
 
 app.listen(3000)
+
+const equipos = JSON.parse(fs.readFileSync('equipos.json', 'utf-8'));
+const pilotos = equipos.map(({ piloto1, piloto2 }) => [piloto1, piloto2]);
+console.log(pilotos.flat());
+
+
+app.get('/', (req, res) => {
+    const pilotos = equipos.map(({ piloto1, piloto2 }) => ({ piloto1, piloto2 }));
+    res.render('index', { pilotos });
+});
+
+
+const circuitos = require('./circuitos.json');
+
+app.get("/carerras", (req, res) => {
+    res.render("carreras", { circuitos });
+});
+
+console.log(circuitos)
+
