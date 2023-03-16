bb84adbc61eb1db1084c1351de0195e62a9b4242
Esteban <eban316@gmail.com>
2023-03-16 13:18:59 -0300

    listaCarreras

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,8 @@
 alert("PRUEBA")
+
+const circuitosDropdown = document.getElementById('circuitos-dropdown');
+
+circuitosDropdown.addEventListener('change', function() {
+  const selectedCircuito = circuitosDropdown.value;
+  console.log('Selected circuito:', selectedCircuito);
+});
