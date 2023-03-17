8a1841bc6185c410258758f0b8fe9dc32a00c593
Eban <eban316@gmail.com>
2023-03-17 00:25:16 -0300

    pruebas

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ const equipos = JSON.parse(fs.readFileSync('equipos.json', 'utf-8'));
 const pilotos = equipos.map(({ piloto1, piloto2 }) => [piloto1, piloto2]);
 console.log(pilotos.flat());
 
+// PILOTOS
 
 app.get('/', (req, res) => {
     const pilotos = equipos.map(({ piloto1, piloto2 }) => ({ piloto1, piloto2 }));
     res.render('index', { pilotos });
 });
 
+// ESCUDERÍAS
 
 const circuitos = require('./circuitos.json');
 
@@ -30,4 +32,29 @@ app.get("/carerras", (req, res) => {
 });
 
 console.log(circuitos)
+
+
+// PRUEBA CON Otoros archivos:
+
+const circuits = require("./circuit.json");
+const pilots = require("./pilots.json");
+
+app.get("/pruebas", (req, res) => {
+    const circuitRaces = circuits.map((circuit) => {
+        const pilotsData = pilots.map((pilot) => {
+            return {
+                id: pilot.id,
+                name: pilot.name,
+                minutes: Math.floor(Math.random() * 61) + 60, // generate random minutes between 60 and 120
+                position: Math.floor(Math.random() * 20) + 1, // generate random position between 1 and 20
+            };
+        });
+        return {
+            id: circuit.id,
+            name: circuit.name,
+            pilots: pilotsData,
+        };
+    });
+    res.render("pruebas", { circuitRaces });
+});
 
