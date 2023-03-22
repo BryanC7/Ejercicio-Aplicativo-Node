a90a5e95f7df57d975a4178c912c452db982bdb8
Andrea Jota <andreacjimenez@gmail.com>
2023-03-22 16:31:13 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const express = require("express");
 const hbs = require("hbs");
-const fs = require("fs")
+const { join } = require("path");
+const path = require("path")
 const data = require("./data/circuitos.json")
 const data1 = require("./data/equipos.json");
 const app = express()
 
 //Settings
-app.set("view engine", "hbs")
 app.set("views", "./views")
-app.set(express.static("public"))
+app.set("view engine", "hbs")
+app.set(express.static(join(__dirname, "/node_modules/bootstrap/dist")))
+app.set(express.static(join(__dirname, "/node_modules/jquery/dist")))
+app.set(express.static("/public"))
+hbs.registerPartials(__dirname + "/views/partials")
+
+
 
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-hbs.registerPartials(__dirname + "./views/partials")
+
+app.disabled("etag")
 app.listen(3000)
 
- 
+
 //Routes
+
 app.get("/", (req, res) => {
-    
+
     let pilotos = data1.equipos.reduce((acc, equipo) => {
         return acc.concat([equipo.piloto1, equipo.piloto2])
-        }, []);
+    }, []);
 
     let banderas = [];
-    console.log(banderas)
-    for (let i = 0; i  < data.carrera.length; i++) {
-    banderas.push(data.carrera[i].bandera);
-    console.log(banderas)
+    
+    for (let i = 0; i < data.carrera.length; i++) {
+        banderas.push(data.carrera[i].bandera);
     }
-    console.log(banderas)
     
     res.render("index", { pilotos, banderas })
 })
@@ -38,14 +44,26 @@ app.get("/", (req, res) => {
 app.get("/ingresar-tiempos", (req, res) => {
     let pilotos = data1.equipos.reduce((acc, equipo) => {
         return acc.concat([equipo.piloto1, equipo.piloto2])
-        }, []);
+    }, []);
 
-        let circuito = [];
+    let circuito = [];
     for (let i = 0; i < data.carrera.length; i++) {
         circuito.push(data.carrera[i].circuito)
     }
 
-    res.render("ingresotiempos", { pilotos, circuito})
+    res.render("ingresotiempos", { pilotos, circuito })
 })
+
+app.get("/ranking", (req, res) => {
+    let escuderia = [];
+    let logo = [];
+    for (let i = 0; i < data1.equipos.length; i++) {
+        escuderia.push(data1.equipos[i].escuderia)
+        logo.push(data1.equipos[i].logo)
+    }
+
+    res.render("ptosescud", { escuderia, logo })
+})
+
 
 
